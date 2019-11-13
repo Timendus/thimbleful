a9fef751af4a1c5f9f98e0ba90074ed02a3e82f4
Tim Franssen <tim.franssen@nedap.com>
2019-11-13 20:54:17 +0100

    Start with plumbing

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'thimbleful.min': './src/index.js',
+    'energize.min':   './src/energize.js',
+    'click.min':      './src/click.js',
+    'filetarget.min': './src/filetarget.js',
+    'router.min':     './src/router.js'
+  },
+
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: '[name].js'
+	},
+
+	devServer: {
+		contentBase: path.join(__dirname, 'docs'),
+		disableHostCheck: true
+	}
+};
